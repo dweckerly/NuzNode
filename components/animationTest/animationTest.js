5531b9d39bb0ab21104d024f3b33f45d1596e8f5
dweckerly <d.weckerly@gmail.com>
2018-08-10 15:13:55 -0500

    more animations

--- components/animationTest/animationTest.js
+++ components/animationTest/animationTest.js
@@ -7,9 +7,14 @@ var animTarget;
 var c = document.getElementById('anim-canvas');
 var ctx = c.getContext('2d');
 
-var startPos = {
+var beginPos = {
     x: 300,
     y: 20
+}
+
+var startPos = {
+    x: beginPos.x,
+    y: beginPos.y
 }
 
 var animImg = { 
@@ -83,8 +88,6 @@ function backSinMove(radius, speed, step) {
     return startPos.y - radius * Math.sin(speed * step);
 }
 
-
-
 function circleForward() {
     animTarget.x = cosMove(60, 0.1, animI) - 60;
     animTarget.y = sinMove(60, 0.1, animI);
@@ -123,6 +126,45 @@ function circleCounterBack() {
         stopAnimation();
     }
     ctx.drawImage(animTarget.img, animTarget.x, animTarget.y, animTarget.w, animTarget.h);
+}
+
+function flash() {
+    animI++;
+    if(animI % 7 == 0) {
+        ctx.globalAlpha = 0.3;
+    }
+    ctx.drawImage(animTarget.img, animTarget.x, animTarget.y, animTarget.w, animTarget.h);
+    ctx.globalAlpha = 1;
+
+    if(animI >= 25) {
+        stopAnimation();
+    }
+}
+
+function grow() {
+    ctx.save();
+    animI = lerp(animI, 0.5, 0.1);
+    ctx.translate(animTarget.x + (animTarget.w / 2), animTarget.y + (animTarget.h / 2));
+    ctx.scale(1 + animI, 1 + animI);
+    ctx.translate(-(animTarget.x + (animTarget.w / 2)), -(animTarget.y + (animTarget.h / 2)));
+    ctx.drawImage(animTarget.img, animTarget.x, animTarget.y, animTarget.w, animTarget.h);
+    ctx.restore();
+    if (animI >= 0.49) {
+        stopAnimation();
+    }
+}
+
+function shrink() {
+    ctx.save();
+    animI = lerp(animI, 0.5, 0.1);
+    ctx.translate(animTarget.x + (animTarget.w / 2), animTarget.y + (animTarget.h / 2));
+    ctx.scale(1 - animI, 1 - animI);
+    ctx.translate(-(animTarget.x + (animTarget.w / 2)), -(animTarget.y + (animTarget.h / 2)));
+    ctx.drawImage(animTarget.img, animTarget.x, animTarget.y, animTarget.w, animTarget.h);
+    ctx.restore();
+    if (animI >= 0.49) {
+        stopAnimation();
+    }
 }
 
 function hop() {
@@ -210,17 +252,106 @@ function backFlip() {
     }
 }
 
+function spinBackward() {
+    ctx.save();
+    animI += 12;
+    ctx.translate(animTarget.x + (animTarget.w / 2), animTarget.y + (animTarget.h / 2));
+    ctx.rotate(animI * Math.PI / 180);
+    ctx.translate(-(animTarget.x + (animTarget.w / 2)), -(animTarget.y + (animTarget.h / 2)));
+    ctx.drawImage(animTarget.img, animTarget.x, animTarget.y, animTarget.w, animTarget.h);
+    ctx.restore();
+    if(animI >= 360) {
+        stopAnimation();
+    }
+}
+
+function spinFastBackward() {
+    ctx.save();
+    animI += 36;
+    ctx.translate(animTarget.x + (animTarget.w / 2), animTarget.y + (animTarget.h / 2));
+    ctx.rotate(animI * Math.PI / 180);
+    ctx.translate(-(animTarget.x + (animTarget.w / 2)), -(animTarget.y + (animTarget.h / 2)));
+    ctx.drawImage(animTarget.img, animTarget.x, animTarget.y, animTarget.w, animTarget.h);
+    ctx.restore();
+    if(animI >= 360) {
+        stopAnimation();
+    }
+}
+
+function spinForward() {
+    ctx.save();
+    animI += 12;
+    ctx.translate(animTarget.x + (animTarget.w / 2), animTarget.y + (animTarget.h / 2));
+    ctx.rotate(-(animI * Math.PI / 180));
+    ctx.translate(-(animTarget.x + (animTarget.w / 2)), -(animTarget.y + (animTarget.h / 2)));
+    ctx.drawImage(animTarget.img, animTarget.x, animTarget.y, animTarget.w, animTarget.h);
+    ctx.restore();
+    if(animI >= 360) {
+        stopAnimation();
+    }
+}
+
+function spinFastForward() {
+    ctx.save();
+    animI += 36;
+    ctx.translate(animTarget.x + (animTarget.w / 2), animTarget.y + (animTarget.h / 2));
+    ctx.rotate(-(animI * Math.PI / 180));
+    ctx.translate(-(animTarget.x + (animTarget.w / 2)), -(animTarget.y + (animTarget.h / 2)));
+    ctx.drawImage(animTarget.img, animTarget.x, animTarget.y, animTarget.w, animTarget.h);
+    ctx.restore();
+    if(animI >= 360) {
+        stopAnimation();
+    }
+}
+
 function shake() {
-    
+    animI++;
+    if(animI <= 10) {
+        animTarget.x = lerp(animTarget.x, startPos.x + 30, 0.1);
+    } else if(animI > 10 && animI <= 20) {
+        animTarget.x = lerp(animTarget.x, startPos.x - 30, 0.1);
+    } else if (animI > 20 && animI <= 30) {
+        animTarget.x = lerp(animTarget.x, startPos.x + 30, 0.1);
+    } else if (animI > 30 && animI <= 40) {
+        animTarget.x = lerp(animTarget.x, startPos.x - 30, 0.1);
+    } else if (animI > 40 && animI <= 50) {
+        animTarget.x = lerp(animTarget.x, startPos.x + 30, 0.1);
+    } else if (animI > 50 && animI <= 60) {
+        animTarget.x = lerp(animTarget.x, startPos.x - 30, 0.1);
+    }
+    if(animI > 60) {
+        stopAnimation();
+    }
+    ctx.drawImage(animTarget.img, animTarget.x, animTarget.y, animTarget.w, animTarget.h);
+}
+
+function shudder() {
+    animI++;
+    if(animI <= 5) {
+        animTarget.x = lerp(animTarget.x, startPos.x + 20, 0.1);
+    } else if(animI > 5 && animI <= 10) {
+        animTarget.x = lerp(animTarget.x, startPos.x - 20, 0.1);
+    } else if (animI > 10 && animI <= 15) {
+        animTarget.x = lerp(animTarget.x, startPos.x + 20, 0.1);
+    } else if (animI > 15 && animI <= 20) {
+        animTarget.x = lerp(animTarget.x, startPos.x - 20, 0.1);
+    } else if (animI > 20 && animI <= 25) {
+        animTarget.x = lerp(animTarget.x, startPos.x + 20, 0.1);
+    } else if (animI > 25 && animI <= 30) {
+        animTarget.x = lerp(animTarget.x, startPos.x - 20, 0.1);
+    }
+    if(animI > 30) {
+        stopAnimation();
+    }
     ctx.drawImage(animTarget.img, animTarget.x, animTarget.y, animTarget.w, animTarget.h);
 }
 
 function lerpToStart() {
-    animTarget.x = lerp(animTarget.x, startPos.x, 0.1);
-    animTarget.y = lerp(animTarget.y, startPos.y, 0.1);
+    animTarget.x = lerp(animTarget.x, beginPos.x, 0.1);
+    animTarget.y = lerp(animTarget.y, beginPos.y, 0.1);
     ctx.drawImage(animTarget.img, animTarget.x, animTarget.y, animTarget.w, animTarget.h);
-    if(animTarget.x >= (startPos.x - 0.1) && animTarget.x <= (startPos.x + 0.1)
-    && animTarget.y >= (startPos.y - 0.1) && animTarget.y <= (startPos.y + 0.1)) {
+    if(animTarget.x >= (beginPos.x - 0.1) && animTarget.x <= (beginPos.x + 0.1)
+    && animTarget.y >= (beginPos.y - 0.1) && animTarget.y <= (beginPos.y + 0.1)) {
         stopAnimation();
     }
 }
@@ -229,8 +360,7 @@ function lerpToLeft() {
     animTarget.x = lerp(animTarget.x, startPos.x - 60, 0.1);
     animTarget.y = lerp(animTarget.y, startPos.y, 0.1);
     ctx.drawImage(animTarget.img, animTarget.x, animTarget.y, animTarget.w, animTarget.h);
-    if(animTarget.x >= (startPos.x - 60.1) && animTarget.x <= (startPos.x - 59.9)
-    && animTarget.y >= (startPos.y - 0.1) && animTarget.y <= (startPos.y + 0.1)) {
+    if(animTarget.x <= (startPos.x - 59.5)) {
         stopAnimation();
     }
 }
@@ -239,8 +369,15 @@ function lerpToRight() {
     animTarget.x = lerp(animTarget.x, startPos.x + 60, 0.1);
     animTarget.y = lerp(animTarget.y, startPos.y, 0.1);
     ctx.drawImage(animTarget.img, animTarget.x, animTarget.y, animTarget.w, animTarget.h);
-    if(animTarget.x >= (startPos.x - 59.9) && animTarget.x <= (startPos.x + 60.1)
-    && animTarget.y >= (startPos.y - 0.1) && animTarget.y <= (startPos.y + 0.1)) {
+    if(animTarget.x >= (startPos.x + 59.5)) {
+        stopAnimation();
+    }
+}
+
+function lerpUp() {
+    animTarget.y = lerp(animTarget.y, startPos.y - 60, 0.1);
+    ctx.drawImage(animTarget.img, animTarget.x, animTarget.y, animTarget.w, animTarget.h);
+    if(animTarget.y <= (startPos.y - 59.5)) {
         stopAnimation();
     }
 }
@@ -277,6 +414,22 @@ function moveFastLeft() {
     }
 }
 
+function moveUp() {
+    animTarget.y -= 5;
+    ctx.drawImage(animTarget.img, animTarget.x, animTarget.y, animTarget.w, animTarget.h);
+    if(animTarget.y <= startPos.y - 60) {
+        stopAnimation();
+    }
+}
+
+function moveFastUp() {
+    animTarget.y -= 25;
+    ctx.drawImage(animTarget.img, animTarget.x, animTarget.y, animTarget.w, animTarget.h);
+    if(animTarget.y <= startPos.y - 120) {
+        stopAnimation();
+    }
+}
+
 function rushRight() {
     animTarget.x = easeInBack(startPos.x, startPos.x + 120, animI, 4);
     animI += 0.08;
@@ -291,6 +444,137 @@ function rushLeft() {
     animI -= 0.08;
     ctx.drawImage(animTarget.img, animTarget.x, animTarget.y, animTarget.w, animTarget.h);
     if(animTarget.x <= startPos.x - 120) {
+        stopAnimation();
+    }
+}
+
+function slideInRight() {
+    ctx.save();
+    animI += 16;
+    ctx.scale(0.5, 0.5);
+    ctx.translate(animTarget.x, animTarget.y);
+    ctx.drawImage(animTarget.img, 0, 0, 2*animTarget.w - (512 - animI), 2*animTarget.h, animTarget.x + (512 - animI), animTarget.y, 2*animTarget.w - (512 - animI), 2*animTarget.h);
+    ctx.translate(-(animTarget.x), -(animTarget.y));
+    ctx.restore();
+    if(animI >= 512) {
+        stopAnimation();
+    }
+}
+
+function slideInLeft() {
+    ctx.save();
+    animI += 16;
+    ctx.scale(0.5, 0.5);
+    ctx.translate(animTarget.x, animTarget.y);
+    ctx.drawImage(animTarget.img, (512 - animI), 0, 2*animTarget.w - (512 - animI), 2*animTarget.h, animTarget.x, animTarget.y, 2*animTarget.w - (512 - animI), 2*animTarget.h);
+    ctx.translate(-(animTarget.x), -(animTarget.y));
+    ctx.restore();
+    if(animI >= 512) {
+        stopAnimation();
+    }
+}
+
+function slideInUp() {
+    ctx.save();
+    animI += 16;
+    ctx.scale(0.5, 0.5);
+    ctx.translate(animTarget.x, animTarget.y);
+    ctx.drawImage(animTarget.img, 0, (512 - animI), 2*animTarget.w, 2*animTarget.h - (512 - animI), animTarget.x, animTarget.y, 2*animTarget.w, 2*animTarget.h - (512 - animI));
+    ctx.translate(-(animTarget.x), -(animTarget.y));
+    ctx.restore();
+    if(animI >= 512) {
+        stopAnimation();
+    }
+}
+
+function slideInDown() {
+    ctx.save();
+    animI += 16;
+    ctx.scale(0.5, 0.5);
+    ctx.translate(animTarget.x, animTarget.y);
+    ctx.drawImage(animTarget.img, 0, 0, 2*animTarget.w, 2*animTarget.h - (512 - animI), animTarget.x, animTarget.y + (512 - animI), 2*animTarget.w, 2*animTarget.h - (512 - animI));
+    ctx.translate(-(animTarget.x), -(animTarget.y));
+    ctx.restore();
+    if(animI >= 512) {
+        stopAnimation();
+    }
+}
+
+function slideOutRight() {
+    ctx.save();
+    animI += 16;
+    ctx.scale(0.5, 0.5);
+    ctx.translate(animTarget.x, animTarget.y);
+    ctx.drawImage(animTarget.img, 0, 0, 2*animTarget.w - animI, 2*animTarget.h, animTarget.x + animI, animTarget.y, 2*animTarget.w - animI, 2*animTarget.h);
+    ctx.translate(-(animTarget.x), -(animTarget.y));
+    ctx.restore();
+    if(animI >= 512) {
+        stopAnimation();
+    }
+}
+
+function slideOutLeft() {
+    ctx.save();
+    animI += 16;
+    ctx.scale(0.5, 0.5);
+    ctx.translate(animTarget.x, animTarget.y);
+    ctx.drawImage(animTarget.img, animI, 0, 2*animTarget.w - animI, 2*animTarget.h, animTarget.x, animTarget.y, 2*animTarget.w - animI, 2*animTarget.h);
+    ctx.translate(-(animTarget.x), -(animTarget.y));
+    ctx.restore();
+    if(animI >= 512) {
+        stopAnimation();
+    }
+}
+
+function slideOutUp() {
+    ctx.save();
+    animI += 16;
+    ctx.scale(0.5, 0.5);
+    ctx.translate(animTarget.x, animTarget.y);
+    ctx.drawImage(animTarget.img, 0, animI, 2*animTarget.w, 2*animTarget.h - animI, animTarget.x, animTarget.y, 2*animTarget.w, 2*animTarget.h - animI);
+    ctx.translate(-(animTarget.x), -(animTarget.y));
+    ctx.restore();
+    if(animI >= 512) {
+        stopAnimation();
+    }
+}
+
+function slideOutDown() {
+    ctx.save();
+    animI += 16;
+    ctx.scale(0.5, 0.5);
+    ctx.translate(animTarget.x, animTarget.y);
+    ctx.drawImage(animTarget.img, 0, 0, 2*animTarget.w, 2*animTarget.h - animI, animTarget.x, animTarget.y + animI, 2*animTarget.w, 2*animTarget.h - animI);
+    ctx.translate(-(animTarget.x), -(animTarget.y));
+    ctx.restore();
+    if(animI >= 512) {
+        stopAnimation();
+    }
+}
+
+
+function wait() {
+    animI++;
+    ctx.drawImage(animTarget.img, animTarget.x, animTarget.y, animTarget.w, animTarget.h);
+    if(animI >= 30) {
+        stopAnimation();
+    }
+}
+
+function invertColor() {
+    animI++;
+    ctx.fillStyle = "black";
+    ctx.fillRect(0, 0, c.width, c.height);
+    ctx.drawImage(animTarget.img, animTarget.x, animTarget.y, animTarget.w, animTarget.h);
+    var imgData = ctx.getImageData(animTarget.x, animTarget.y, animTarget.w, animTarget.h);
+    var data = imgData.data;
+    for(let i = 0; i < data.length; i += 4) {
+        data[i] = 255 - data[i];
+        data[i + 1] = 255 - data[i + 1];
+        data[i + 2] = 255 - data[i + 2];
+    }
+    ctx.putImageData(imgData, animTarget.x, animTarget.y);
+    if(animI >= 30) {
         stopAnimation();
     }
 }
@@ -310,14 +594,23 @@ function runAnimQueue() {
         case 'circlecounterb':
             circleCounterBack();
             break;
+        case 'flash':
+            flash();
+            break;
         case 'flipB':
             backFlip();
             break;
         case 'flipF':
             frontFlip();
             break;
+        case 'grow':
+            grow();
+            break;
         case 'hop':
             hop(); 
+            break;
+        case 'invertcolor':
+            invertColor();
             break;
         case 'jump':
             jump();
@@ -337,6 +630,9 @@ function runAnimQueue() {
         case 'ltoleft' :
             lerpToLeft();
             break;
+        case 'lerpup':
+            lerpUp();
+            break;
         case 'ltoright' :
             lerpToRight();
             break;
@@ -355,11 +651,65 @@ function runAnimQueue() {
         case 'movefl':
             moveFastLeft();
             break;
+        case 'moveu':
+            moveUp();
+            break;
+        case 'movefu':
+            moveFastUp();
+            break;
         case 'rushr':
             rushRight();
             break;
         case 'rushl':
             rushLeft();
+            break;
+        case 'shake':
+            shake();
+            break;
+        case 'shrink':
+            shrink();
+            break;
+        case 'shudder':
+            shudder();
+            break;
+        case 'spinb':
+            spinBackward();
+            break;
+        case 'spinf':
+            spinForward();
+            break;
+        case 'spinfb':
+            spinFastBackward();
+            break;
+        case 'spinff':
+            spinFastForward();
+            break;
+        case 'slideir':
+            slideInRight();
+            break;
+        case 'slideil':
+            slideInLeft();
+            break;
+        case 'slideiu':
+            slideInUp();
+            break;
+        case 'slideid':
+            slideInDown();
+            break;
+        case 'slideor':
+            slideOutRight();
+            break;
+        case 'slideol':
+            slideOutLeft();
+            break;
+        case 'slideou':
+            slideOutUp();
+            break;
+        case 'slideod':
+            slideOutDown();
+            break;
+        case 'wait':
+            wait();
             break;
         default:
             ctx.drawImage(animTarget.img, animTarget.x, animTarget.y, animTarget.w, animTarget.h);
@@ -367,6 +717,8 @@ function runAnimQueue() {
     }
 }
 function nextAnim() {
+    startPos.x = animTarget.x;
+    startPos.y = animTarget.y;
     animation = animQueue.shift();
 }
 
@@ -378,8 +730,8 @@ function stopAnimation() {
     } else {
         console.log("stopped");
         animating = false;
-        animImg.x = startPos.x;
-        animImg.y = startPos.y;
+        animImg.x = beginPos.x;
+        animImg.y = beginPos.y;
         animImg.w = 256;
         animImg.h = 256;
         $('#anim-btn-basic').prop('disabled', false);
@@ -390,8 +742,8 @@ function stopAnimation() {
 function startAnimation() {
     $('#anim-btn-basic').prop('disabled', true);
     $('#anim-btn-chain').prop('disabled', true);
-    nextAnim();
     animTarget = animImg;
+    nextAnim();
     animating = true;
 }
 
