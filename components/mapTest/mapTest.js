f12a9a50363e4361cb130b381e54b41c434d639f
David <d.weckerly@gmail.com>
2018-09-10 05:40:51 -0500

    added vars

--- components/mapTest/mapTest.js
+++ components/mapTest/mapTest.js
@@ -1,3 +1,10 @@
+var c = document.getElementById('main-canvas');
+var ctx = c.getContext('2d');
+
 $('#back').click(() => {
     changeSection('main', titleComp);
 });
+
+function clearCanvas() {
+    ctx.clearRect(0, 0, c.width, c.height);
+}
