2c5cb7ee33a9d17a4b26b479ccccb06ebe7611ca
David <d.weckerly@gmail.com>
2018-09-12 06:24:35 -0500

    changed rect method to fillRect

--- components/mapTest/mapTest.js
+++ components/mapTest/mapTest.js
@@ -1,6 +1,6 @@
 var c = document.getElementById('main-canvas');
 var ctx = c.getContext('2d');
-var cOffset = {x: 0, y: 0};
+var cOffset = { x: 0, y: 0 };
 
 var mouseOffset = { x: 0, y: 0 };
 var mousePos = { x: 0, y: 0 };
@@ -27,7 +27,7 @@ $('#back').click(() => {
 
 $('#clear').click(() => {
     clearCanvas();
-    if(clear) {
+    if (clear) {
         clear = false;
         $('#clear').html("Clear");
     } else {
@@ -62,6 +62,8 @@ c.addEventListener('mousemove', function(evt) {
         cOffset.y += mousePos.y - mouseOffset.y;
         mouseOffset.x = mousePos.x;
         mouseOffset.y = mousePos.y;
+        clearCanvas();
+        draw();
     }
 }, false);
 
@@ -78,22 +80,22 @@ function createMap() {
 */
 
 function createSection(num) {
-    if(num >= sectionMax || num == 1) {
+    if (num >= sectionMax || num == 1) {
         var nodeCount = 3;
     } else {
         var nodeCount = Math.floor(Math.random() * 3) + 3;
     }
     var nodes = [];
-    for(let i = 0; i < nodeCount; i++){
+    for (let i = 0; i < nodeCount; i++) {
         nodes.push(createNode());
     }
     return section;
 }
 
 function createNode() {
-    if(map.length == 0) {
+    if (map.length == 0) {
         // first section
-    } else if(map.length == sectionMax - 1) {
+    } else if (map.length == sectionMax - 1) {
         // last section
     } else {
         // general section
@@ -102,13 +104,13 @@ function createNode() {
 }
 
 function createMap() {
-    for(let i = 0; i < 15; i++) {
+    for (let i = 0; i < 15; i++) {
         let fx = Math.round(Math.random() * 10) - 20;
         let fy = Math.round(Math.random() * 60) + 20;
-        if(i != 0) {
-            nodes.push({x: (nodes[i - 1].x + fx), y: (nodes[i - 1].y + fy)});
+        if (i != 0) {
+            nodes.push({ x: (nodes[i - 1].x + fx), y: (nodes[i - 1].y + fy) });
         } else {
-            nodes.push({x: fx + 400, y: fy});
+            nodes.push({ x: fx + 400, y: fy });
         }
     }
     console.log(nodes);
@@ -119,19 +121,16 @@ function createMap() {
 function baseNode() {
     let x = c.width / 2 - 10 + cOffset.x;
     let y = c.height - 60 + cOffset.y;
-    ctx.rect(x, y, 20, 20);
-    ctx.fill();
+    ctx.fillRect(x, y, 20, 20);
 }
 
-function endNode(){
+function endNode() {
     let x = c.width / 2 - 10 + cOffset.x;
     let y = 0 + cOffset.y;
-    ctx.rect(x, y, 20, 20);
-    ctx.fill();
+    ctx.fillRect(x, y, 20, 20);
 }
 
 function draw() {
-    clearCanvas();
     baseNode();
     endNode();
     /*
@@ -141,12 +140,5 @@ function draw() {
     }
     */
 }
-function main() {
-    var main = setInterval(function() {
-        if(!clear) {
-            draw();
-        }
-    }, 20);
-}
 
-main();
+draw();
