b14655646d8748cd0fa24435a0cc24f62831d8e4
David <d.weckerly@gmail.com>
2018-09-15 06:48:35 -0500

    added variable for node dimension

--- components/mapTest/mapTest.js
+++ components/mapTest/mapTest.js
@@ -11,6 +11,8 @@ var map = [];
 var sections = [];
 var nodes = [];
 
+var nodeDim = 50;
+
 var sectionMax = 7;
 var sectionWidth = 600;
 var sectionHeight = 200;
@@ -99,55 +101,55 @@ function resetOffset() {
 }
 
 function createBaseNode() {
-    bNode.x = c.width / 2 - 10 + cOffset.x;
-    bNode.y = (sectionHeight * sectionMax) + 20 + cOffset.y;
+    bNode.x = (c.width / 2) - (nodeDim / 2) + cOffset.x;
+    bNode.y = (sectionHeight * sectionMax) + nodeDim + cOffset.y;
 }
 
 function baseNode() {
-    ctx.fillRect(bNode.x + cOffset.x, bNode.y + cOffset.y, 20, 20);
+    ctx.fillRect(bNode.x + cOffset.x, bNode.y + cOffset.y, nodeDim, nodeDim);
 }
 
 function createEndNode() {
-    eNode.x = c.width / 2 - 10 + cOffset.x;
+    eNode.x = (c.width / 2) - (nodeDim / 2) + cOffset.x;
     eNode.y = 0 + cOffset.y;
 }
 
 function endNode() {
-    ctx.fillRect(eNode.x + cOffset.x, eNode.y + cOffset.y, 20, 20);
+    ctx.fillRect(eNode.x + cOffset.x, eNode.y + cOffset.y, nodeDim, nodeDim);
 }
 
 function createSections() {
     sections = [];
     //let sectionHeight = (bNode.y - eNode.y - 20) / sectionMax;
-    for(let i = 0; i < sectionMax; i++) {
-        sections.push({x: c.width / 2 - sectionWidth / 2 + cOffset.x, y: sectionHeight * i + eNode.y + 20, w: sectionWidth, h: sectionHeight});
+    for (let i = 0; i < sectionMax; i++) {
+        sections.push({ x: c.width / 2 - sectionWidth / 2 + cOffset.x, y: sectionHeight * i + eNode.y + nodeDim, w: sectionWidth, h: sectionHeight });
     }
 }
 
 function drawSections() {
-    for(let i = 0; i < sections.length; i++) {
+    for (let i = 0; i < sections.length; i++) {
         ctx.strokeRect(sections[i].x + cOffset.x, sections[i].y + cOffset.y, sections[i].w, sections[i].h);
     }
 }
 
 function createNodes() {
     nodes = [];
-    for(let i = 0; i < sections.length; i++) {
+    for (let i = 0; i < sections.length; i++) {
         let r;
-        if(i > 0 && i < sections.length - 1) {
+        if (i > 0 && i < sections.length - 1) {
             r = Math.floor(Math.random() * 3) + 3;
         } else {
             r = 3;
         }
         let nodeSectionWidth = sections[i].w / r;
-        for(let j = 0; j < r; j++) {
-            let x = sections[i].x + nodeSectionWidth * j; 
-            let w = sections[i].x + nodeSectionWidth * (j+1);
+        for (let j = 0; j < r; j++) {
+            let x = sections[i].x + nodeSectionWidth * j;
+            let w = sections[i].x + nodeSectionWidth * (j + 1);
             let y = sections[i].y;
             let h = sections[i].h;
 
-            let nodeX = (x + ((w - x) / 2)) - 10; 
-            let nodeY = (y + h / 2) - 10;
+            let nodeX = (x + ((w - x) / 2)) - (nodeDim / 2);
+            let nodeY = (y + h / 2) - (nodeDim / 2);
 
             let section = i;
             nodes.push({
@@ -160,8 +162,8 @@ function createNodes() {
 }
 
 function drawNodes() {
-    for(let i = 0; i < nodes.length; i++) {
-        ctx.strokeRect(nodes[i].x + cOffset.x, nodes[i].y + cOffset.y, 20, 20);
+    for (let i = 0; i < nodes.length; i++) {
+        ctx.strokeRect(nodes[i].x + cOffset.x, nodes[i].y + cOffset.y, nodeDim, nodeDim);
     }
 }
 
